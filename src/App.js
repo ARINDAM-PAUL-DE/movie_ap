8669ef8e9f9ddf463c1593925715850a3fd44d09
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-11-10 12:59:11 +0530

    file structure and router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <Router>
+        <Routes>
+          <Route index element={<h1>Home page</h1>}></Route>
+          <Route path='movie/:id' element={<h1>Movie page</h1>}></Route>
+          <Route path='movie/:type' element={<h1>Movie Type</h1>}></Route>
+          <Route path='/*' element ={<h1>Error Page</h1>}></Route>
+        </Routes>
+     </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
