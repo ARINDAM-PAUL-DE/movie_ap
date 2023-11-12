a89d5a704782543d0dd6480a8554d401178d1e34
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-11-12 11:59:07 +0530

    added some value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react'
 import './App.css'
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div className='App'>
-     <Router>
+    <Router>
+    <Header/>
         <Routes>
-          <Route index element={<h1>Home page</h1>}></Route>
+          <Route index element={<Home/>}></Route> 
           <Route path='movie/:id' element={<h1>Movie page</h1>}></Route>
           <Route path='movie/:type' element={<h1>Movie Type</h1>}></Route>
           <Route path='/*' element ={<h1>Error Page</h1>}></Route>
         </Routes>
-     </Router>
+    </Router>
     </div>
   )
 }
